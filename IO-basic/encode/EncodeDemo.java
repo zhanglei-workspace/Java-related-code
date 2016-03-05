817b6273e1f6d1d29529e5166afb088edc1712bb
SuperHKM <superhookm@gmail.com>
2016-03-05 22:48:22 +0800

    GBK->UTF-8

--- IO-basic/encode/EncodeDemo.java
+++ IO-basic/encode/EncodeDemo.java
@@ -26,6 +26,11 @@ public class EncodeDemo
 		String str1 = new String(buf,"UTF-8");
 		
 		System.out.println(str1);
+		
+		
+		/*//实现GBK编码字节流到UTF-8编码字节流的转换
+		byte[] src,dst;
+		dst=new String(src,"GBK").getBytes("UTF-8");*/
 	}
 
 	public static void printBuf(byte[] buf)
